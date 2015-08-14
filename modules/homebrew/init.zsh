859c5865faa459cd32b96dd31e33c158347aadad
Glenn Jorde <glenn@tregusti.com>
2015-08-14 22:06:53 +0200

    feat(homebrew): Load home brew and cask aliases.

--- modules/homebrew/init.zsh
+++ modules/homebrew/init.zsh
@@ -15,19 +15,19 @@ fi
 #
 
 # Homebrew
-alias brewc='brew cleanup'
-alias brewC='brew cleanup --force'
-alias brewi='brew install'
-alias brewl='brew list'
-alias brews='brew search'
+# alias brewc='brew cleanup'
+# alias brewC='brew cleanup --force'
+# alias brewi='brew install'
+# alias brewl='brew list'
+# alias brews='brew search'
 alias brewu='brew update && brew upgrade --all'
-alias brewx='brew remove'
+# alias brewx='brew remove'
 
 # Homebrew Cask
 alias cask='brew cask'
-alias caskc='brew cask cleanup --outdated'
-alias caskC='brew cask cleanup'
-alias caski='brew cask install'
-alias caskl='brew cask list'
-alias casks='brew cask search'
-alias caskx='brew cask uninstall'
+# alias caskc='brew cask cleanup --outdated'
+# alias caskC='brew cask cleanup'
+# alias caski='brew cask install'
+# alias caskl='brew cask list'
+# alias casks='brew cask search'
+# alias caskx='brew cask uninstall'
